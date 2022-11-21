3f3213c323a44d85c55018cae0b71dc0aef4746d
Dmytro Oseledko <d.oseledko@outlook.com>
2022-11-21 12:30:31 +0200

    sys.argv

--- hw6_sort.py
+++ hw6_sort.py
@@ -1,2 +1,14 @@
 # python-hw6-sort - Домашнее задание 6 
 # Скрипт должен проходить по указанной во время вызова папке и сортировать все файлы по группам
+
+
+import sys
+from pathlib import Path
+
+
+# Скрипт принимает один аргумент при запуске — это имя папки
+# path_folders = []
+
+path_folders = sys.argv[-1]
+#print(path_folders)
+
